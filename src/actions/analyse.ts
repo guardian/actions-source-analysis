fde58161ba3e4f7d00aa0abe3dc9a87c64262ab9
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-08-20 13:32:23 +0100

    add analyser action

---
+++ src/actions/analyse.ts
@@ -0,0 +1,45 @@
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import { info } from '@actions/core';
+import { exec } from '@actions/exec';
+
+export const analyse = async (): Promise<void> => {
+	writeConfigFile();
+	await scan();
+};
+
+// TODO: Make parts of config configurable
+/* eslint-disable no-useless-escape -- It's not actually useless */
+const config = `
+module.exports = {
+	crawlFrom: '../',
+	globs: ['**/(components|pages)/**/!(*.test|*.spec).@(js|ts)?(x)'],
+	includeSubComponents: true,
+	importedFrom: /^(@guardian\/src-).*/,
+	getComponentName: ({ imported, moduleName }) => {
+		return moduleName + imported;
+	},
+	processors: [
+		['count-components-and-props', {}],
+		['raw-report', { outputTo: '../.source/source-analysis.json' }],
+	],
+};
+`;
+/* eslint-enable no-useless-escape -- It's not actually useless */
+
+const writeConfigFile = () => {
+	if (existsSync('.source')) {
+		// TODO: Handle this better
+		throw new Error('A .source directory already exists');
+	}
+
+	info('Creating .source directory');
+	mkdirSync('.source');
+
+	info('Writing config file');
+	writeFileSync('.source/config.js', config);
+};
+
+const scan = async (): Promise<void> => {
+	info('Running react-scanner');
+	await exec('npx react-scanner -c .source/config.js');
+};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { debug, info, setFailed } from '@actions/core';
 import { context } from '@actions/github';
+import { analyse } from './actions/analyse';
 import { name } from './lib/pkg';
 
-function run(): void {
+async function run(): Promise<void> {
 	try {
 		info(`Running ${name}`);
 
 		debug(`Event name: ${context.eventName}`);
 		debug(`Action type: ${context.payload.action ?? 'Unknown'}`);
+
+		await analyse();
 	} catch (error) {
 		if (error instanceof Error) {
 			setFailed(error.message);
