36e75830a15ffbe2f3b973f99a27f5e560ceb08d
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-08-20 12:51:46 +0100

    remove need for GH token

--- src/lib/github.ts
+++
@@ -1,5 +0,0 @@
-import { getInput } from '@actions/core';
-import { getOctokit } from '@actions/github';
-
-export const token = getInput('github-token', { required: true });
-export const octokit = getOctokit(token);
